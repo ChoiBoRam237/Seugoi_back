bcd98dbc05533c25b96658a813436f708d3f77cc
ChoiBoRam237 <choiboram@gmail.com>
2026-06-11 13:59:39 +0900

    init

---
+++ src/main/java/com/example/seugoi_back/SeugoiBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.seugoi_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeugoiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeugoiBackApplication.class, args);
+	}
+
+}
